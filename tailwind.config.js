3678dd27ac1e40f6ee5aaa016c94b6a2c4242aa5
nikolamrsic <103537255+nikolamrsic@users.noreply.github.com>
2023-01-16 14:19:54 +0100

    src to public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.js"],
+  content: ["./public/**/*.js"],
   theme: {
     extend: {
       colors: {
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const tailwindCss = require("tailwindcss");
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./public/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.js",
@@ -12,7 +12,7 @@ module.exports = {
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: "./src/index.html",
+      template: "./public/index.html",
     }),
   ],
 
